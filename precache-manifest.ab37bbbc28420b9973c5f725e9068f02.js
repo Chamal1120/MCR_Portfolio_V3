efe3d8602f84ce801e49c91e6bf8de3e0cc10ccc
chamal1120 <kenway213beep@gmail.com>
2023-06-25 23:22:58 +0530

    Updates

---
+++ precache-manifest.ab37bbbc28420b9973c5f725e9068f02.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59fe8cc5ee6d1c41714355b11abf25e8",
+    "url": "/MCR_Portfolio_V3/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/MCR_Portfolio_V3/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3fcd49def4016a97e07f",
+    "url": "/MCR_Portfolio_V3/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/MCR_Portfolio_V3/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/MCR_Portfolio_V3/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fcd49def4016a97e07f",
+    "url": "/MCR_Portfolio_V3/static/js/main.f6003589.chunk.js"
+  },
+  {
+    "revision": "b44cef0786b276df5a08",
+    "url": "/MCR_Portfolio_V3/static/js/runtime-main.2359d29b.js"
+  },
+  {
+    "revision": "028e15adfa6bd6d44927ddd63f106fe1",
+    "url": "/MCR_Portfolio_V3/static/media/resume.028e15ad.pdf"
+  }
+]);
